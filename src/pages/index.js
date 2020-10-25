759eace71f19269f855af557ddae0efa0373625c
Dennis Augustine <dvaugustine@gmail.com>
2020-10-25 15:20:38 -0400

    Updated cms edit page to be same as index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,42 @@
-import React from "react"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, Link, graphql } from 'gatsby'
 
-export default function Home() {
-  return <div>Hello world!</div>
+
+export const IndexPageTemplate = ({title}) => (
+  <div>
+    {title}
+  </div>
+)
+
+IndexPageTemplate.propTypes = {
+  title: PropTypes.string,
 }
+
+const IndexPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark
+
+  return (
+    <IndexPageTemplate title={frontmatter.title} />
+  )
+}
+
+IndexPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
+}
+
+export default IndexPage
+
+export const pageQuery = graphql`
+  query IndexPageTemplate {
+    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+      frontmatter {
+        title
+      }
+    }
+  }
+`
